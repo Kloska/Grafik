394d9648231500af5f95e4a4573a6087b1401056
Kloska <56342396+Kloska@users.noreply.github.com>
2021-03-19 11:10:09 +0100

    Lagt in de nya uppdaterade filerna

--- src/SimpelGrafik.java
+++ src/SimpelGrafik.java
@@ -8,41 +8,68 @@ import java.awt.*;
  * @author Magnus Silverdal
  */
 public class SimpelGrafik extends Canvas {
+    private int width = 800;
+    private int height = 600;
+
+    private int houseX, houseY;
+    private int houseVX, houseVY;
+
     public SimpelGrafik() {
         JFrame frame = new JFrame("A simple painting");
-        this.setSize(800,600);
+        this.setSize(width, height);
         frame.add(this);
         frame.pack();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
+
+        houseX = 300;
+        houseY = 150;
+        houseVX = 1;
+        houseVY = 0;
+    }
+
+    public void update() {
+        houseX += houseVX;
+        if (houseX > width) {
+            houseVX = -1;
+        }
+        if (houseX < 0) {
+            houseVX = 1;
+        }
     }
 
     public void paint(Graphics g) {
-        g.setColor(Color.black);
-        g.fillRect(200,200,80,20);
-        g.setColor(new Color(255,127,0));
-        g.drawOval(200,200,80,20);
-        drawTree(g, 100,200);
-        drawTree(g, 110,200);
-        drawTree(g, 120,200);
-        drawTree(g, 130,200);
-        drawTree(g, 140,200);
-        drawTree(g, 150,200);
-        drawHouse(g, 170,250);
-    }
-
-    private void drawHouse(Graphics g, int x, int y) {
-        g.setColor(Color.red);
-        g.fillRect(80,80,100,110);
+        update();
+        drawHouse(g, houseX, houseY);
+        drawTree(g, 100, 200);
+        drawTree(g, 110, 200);
+        drawTree(g, 120, 200);
+        drawTree(g, 130, 200);
+        drawTree(g, 140, 200);
+        drawTree(g, 150, 200);
+        repaint();
     }
 
     private void drawTree(Graphics g, int x, int y) {
-        g.setColor(new Color(0,128,0));
-        int[] xs = {0+x, 10+x, 20+x};
-        int[] ys = {30+y,0+y,30+y};
-        g.fillPolygon(xs,ys,3);
-        g.setColor(new Color(200,128,30));
-        g.fillRect(7+x,30+y,6,10);
+        g.setColor(new Color(0, 128, 0));
+        int[] xs = {0 + x, 10 + x, 20 + x};
+        int[] ys = {30 + y, 0 + y, 30 + y};
+        g.fillPolygon(xs, ys, 3);
+        g.setColor(new Color(200, 128, 30));
+        g.fillRect(7 + x, 30 + y, 6, 10);
+    }
+
+    private void drawHouse(Graphics g, int x, int y) {
+        g.setColor(new Color(0xAA1111));
+        g.fillRect(x, y - 40, 50, 40);
+        g.setColor(new Color(0x444444));
+        int[] xcoords = {x - 5, x + 25, x + 55};
+        int[] ycoords = {y - 40, y - 65, y - 40};
+        g.fillPolygon(xcoords, ycoords, 3);
+        g.fillRect(x + 4, y - 35, 15, 35);
+        g.drawRect(x + 25, y - 30, 20, 20);
+        g.setColor(new Color(0xFFA3DCFA));
+        g.fillRect(x + 26, y - 29, 18, 18);
     }
 
     public static void main(String[] args) {
